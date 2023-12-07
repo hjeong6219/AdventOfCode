232e7a81767490adb3de500bfa667a444314facc
hjeong6219 <it.josh.jeong@gmail.com>
2023-12-07 01:54:33 -0500

    completed day 1

---
+++ 2023/day1/part2.py
@@ -0,0 +1,31 @@
+filename = "data.txt"
+input = open(filename,'r').read().split('\n')
+
+nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+numChar = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+total = 0
+
+for line in input:
+  digits = []
+  n = 0
+  for index, char in enumerate(line):
+    if char in nums:
+      if n == 0 or n == 1:
+        digits.append(char)
+        n += 1
+      if n == 2:
+        digits[-1] = char
+    else:
+      for i, c in enumerate(numChar):
+        if line[index:].startswith(c):
+          if n == 0 or n == 1:
+            digits.append(str(i + 1))
+            n += 1
+          if n == 2:
+            digits[-1] = (str(i + 1))
+    if index == len(line) - 1:
+      if n == 1:
+        digits.append(digits[0])
+  total += int(digits[0] + digits[1])
+
+print(total)
